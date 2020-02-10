053b2d8e0c4808b2d4cd6d3fa0b7a3cad0441221
Ilpo Järvinen <ilpo.jarvinen@cs.helsinki.fi>
2020-02-10 14:48:29 +0200

    Remove delayed ACK sysctl

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -5266,12 +5266,8 @@ send_now:
 	}
 
 	if (!ofo_possible || RB_EMPTY_ROOT(&tp->out_of_order_queue)) {
-		if (sock_net(sk)->ipv4.sysctl_tcp_delayed_acks) {
-			tcp_send_delayed_ack(sk);
-			return;
-		}
-		/* Delayed acks disabled */
-		goto send_now;
+		tcp_send_delayed_ack(sk);
+		return;
 	}
 
 	if (!tcp_is_sack(tp) ||
